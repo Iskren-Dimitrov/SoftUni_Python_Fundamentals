d9232b5d771d735e4255126ed6a1e038fddb86b6
Iskren Dimitrov <114501449+Iskren-Dimitrov@users.noreply.github.com>
2022-12-09 14:39:21 +0200

    Delete exercise_basic_syntax_conditional_statements_and_loops directory

--- exercise_basic_syntax_conditional_statements_and_loops/maximum_multiple.py
+++
@@ -1,6 +0,0 @@
-first_number = int(input())
-second_number = int(input())
-for current_number in range(second_number, first_number, -1):
-    if current_number % first_number == 0:
-        break
-print(current_number)
